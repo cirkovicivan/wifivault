a07b1408368af4c0cb7e55c328030164b15218ec
cirko0 <cirko007ivan@gmail.com>
2025-10-29 23:20:25 +0100

    extract profile names (SSIDs)

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import subprocess
 # text - converts the output from bytes to string
 # capture_output - captures the output so it is not directly displayed in terminal so we can parse it (it is in output.stdout)
 output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True, text=True)
+
+SSID = []
+
+# Splits the output into lines and from each line extracts the SSID
+for line in output.stdout.splitlines():
+    if "All User Profile" in line:
+        SSID.append(line.split(":")[1].strip())
